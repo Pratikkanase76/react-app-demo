91b7ffe6b5dc8f1751c3bc764f70f50a541cc46a
Pratikkanase76 <kanase.pratik007@gmail.com>
2021-07-28 23:28:36 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+        repellendus exercitationem ratione nihil repellat laborum pariatur
+        impedit nisi nesciunt culpa consequatur perferendis nemo molestias quas,
+        quaerat facilis dolores. Nemo, laboriosam.
+      </p>
     </div>
   );
 }
