53caf03e77cee40887ef174fda8fc570c3b8fd83
Pratikkanase76 <kanase.pratik007@gmail.com>
2021-07-30 19:36:23 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,108 +1,40 @@
+import axios from "axios";
 import React, { useState } from "react";
-import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      <MyComponent />
-    </div>
-  );
-};
+  const [list, setList] = useState([]);
+  const ajaxCall = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
 
-function MyComponent() {
-  const [userList, setUserList] = useState([{}]);
+    const result = await axios.get(url);
+    console.log(result.data);
 
-  const [userName, setUserName] = useState("");
-  const userNameHandler = (e) => setUserName(e.target.value);
-
-  const [userWork, setUserWork] = useState("");
-  const userWorkHandler = (e) => setUserWork(e.target.value);
-
-  const [userEmail, setUserEmail] = useState("");
-  const userEmailHandler = (e) => setUserEmail(e.target.value);
-
-  const [userPassword, setUserPassword] = useState("");
-  const userPasswordHandler = (e) => setUserPassword(e.target.value);
-
-  const addUser = () => {
-    const newUser = {
-      name: userName,
-      work: userWork,
-      email: userEmail,
-      password: userPassword,
-    };
-
-    const newUserList = [newUser, ...userList];
-    setUserList(newUserList);
-
-    setUserName("");
-    setUserWork("");
-    setUserEmail("");
-    setUserPassword("");
+    setList(result.data);
   };
 
   return (
     <div>
-      <h1 className="bg-dark text-light p-3 w-100">User Registration</h1>
-
-      <form>
-        <div>
-          <input
-            type="text"
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter your name"
-            value={userName}
-            onChange={userNameHandler}
-          />
-          <input
-            type="text"
-            className="form-control form-control-lg mb-1"
-            placeholder="work"
-            value={userWork}
-            onChange={userWorkHandler}
-          />
-          <input
-            type="email"
-            className="form-control form-control-lg mb-1"
-            placeholder="Email address"
-            value={userEmail}
-            onChange={userEmailHandler}
-          />
-          <input
-            type="password"
-            className="form-control form-control-lg mb-1"
-            placeholder="Password"
-            value={userPassword}
-            onChange={userPasswordHandler}
-          />
-
-          <input
-            type="button"
-            className="btn btn-lg btn-secondary w-100"
-            value="Register"
-            onClick={addUser}
-          />
-        </div>
-      </form>
+      <h1>Working with AJAX</h1>
+      <input type="button" value="AJAX Call" onClick={ajaxCall} />
 
       <div>
-        {userList.map((item) => {
+        {list.map((item) => {
           return (
-            <table className="table table-dark table-striped">
+            <table class="table">
               <thead>
                 <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">Work</th>
-                  <th scope="col">Email Id</th>
-                  <th scope="col">Password</th>
+                  <th scope="col">UserId</th>
+                  <th scope="col">ID</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Body</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>{item.name}</td>
-                  <td>{item.work}</td>
-                  <td>{item.email}</td>
-                  <td>{item.password}</td>
+                  <td>{item.userId}</td>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.body}</td>
                 </tr>
               </tbody>
             </table>
@@ -111,5 +43,6 @@ function MyComponent() {
       </div>
     </div>
   );
-}
+};
+
 export default App;
