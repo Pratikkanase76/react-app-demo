797ad727c29675f4c4f6e3a7b568222941f28225
Pratikkanase76 <kanase.pratik007@gmail.com>
2021-07-30 18:05:29 +0530

    user Registration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,110 @@
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
-function App() {
-  //react hooks
-  const [list, setList] = useState([]);
-
-  const addNewHello = () => {
-    const newList = [...list, "Hello"];
-    setList(newList);
-  };
-
-  console.log(list);
-  //list.map always put in {}
+const App = () => {
   return (
     <div>
-      <h1>Working with Events and List</h1>
+      <MyComponent />
+    </div>
+  );
+};
 
-      <input type="button" value="Say Hello" onClick={addNewHello} />
+function MyComponent() {
+  const [userList, setUserList] = useState([{}]);
 
-      {list.map((item) => {
-        return <h3>{item}</h3>;
-      })}
+  const [userName, setUserName] = useState("");
+  const userNameHandler = (e) => setUserName(e.target.value);
+
+  const [userWork, setUserWork] = useState("");
+  const userWorkHandler = (e) => setUserWork(e.target.value);
+
+  const [userEmail, setUserEmail] = useState("");
+  const userEmailHandler = (e) => setUserEmail(e.target.value);
+
+  const [userPassword, setUserPassword] = useState("");
+  const userPasswordHandler = (e) => setUserPassword(e.target.value);
+
+  const addUser = () => {
+    const newUser = {
+      name: userName,
+      work: userWork,
+      email: userEmail,
+      password: userPassword,
+    };
+
+    const newUserList = [newUser, ...userList];
+    setUserList(newUserList);
+  };
+
+  return (
+    <div>
+      <h1 className="bg-dark text-light p-3 w-100">User Registration</h1>
+
+      <form>
+        <div>
+          <input
+            type="text"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter your name"
+            value={userName}
+            onChange={userNameHandler}
+          />
+          <input
+            type="text"
+            className="form-control form-control-lg mb-1"
+            placeholder="work"
+            value={userWork}
+            onChange={userWorkHandler}
+          />
+          <input
+            type="email"
+            className="form-control form-control-lg mb-1"
+            placeholder="Email address"
+            value={userEmail}
+            onChange={userEmailHandler}
+          />
+          <input
+            type="password"
+            className="form-control form-control-lg mb-1"
+            placeholder="Password"
+            value={userPassword}
+            onChange={userPasswordHandler}
+          />
+
+          <input
+            type="button"
+            className="btn btn-lg btn-secondary w-100"
+            value="Register"
+            onClick={addUser}
+          />
+        </div>
+      </form>
+
+      <div>
+        {userList.map((item) => {
+          return (
+            <table className="table table-dark table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Work</th>
+                  <th scope="col">Email Id</th>
+                  <th scope="col">Password</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.work}</td>
+                  <td>{item.email}</td>
+                  <td>{item.password}</td>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
